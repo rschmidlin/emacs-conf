37fc67cf500a2df9cbc4832101f1c5fb7cd94956
Raul Schmidlin <raulschmidlin@gmail.com>
2017-09-12 19:53:44 +0200

    Adapting xah-fly-keys

--- input.el
+++ input.el
@@ -1,29 +1,31 @@
 ; Initialize ErgoEmacs, requires persistent-soft and undo-tree (at directory .emacs.d)
-(use-package ergoemacs-mode
-	     :ensure t
- 	     :pin melpa
- 	     :init
- 	     (setq ergoemacs-theme nil)
- 	     ;(setq ergoemacs-keyboard-layout "programmer-dv")
- 	     (setq ergoemacs-keyboard-layout "de")
- 	     :config
- 	     (ergoemacs-mode 1))
+;; (use-package ergoemacs-mode
+;; 	     :ensure t
+;;  	     :pin melpa
+;;  	     :init
+;;  	     (setq ergoemacs-theme nil)
+;;  	     ;(setq ergoemacs-keyboard-layout "programmer-dv")
+;;  	     (setq ergoemacs-keyboard-layout "de")
+;;  	     :config
+;;  	     (ergoemacs-mode 1))
 
-(use-package god-mode
-  :ensure t
-  :pin melpa
-  :bind ("<escape>" . god-local-mode)
-  :init
-  (setq god-exempt-major-modes nil)
-  (setq god-exempt-predicates nil))
-
-;; (use-package xah-fly-keys
+;; (use-package god-mode
 ;;   :ensure t
 ;;   :pin melpa
-;;   :config
-;;   (xah-fly-keys-set-layout "qwerty") ; required if you use qwerty
-;; 										; (xah-fly-set-layout "dvorak") ; by default, it's dvorak
-;;   (xah-fly-keys 1))
+;;   :bind ("<escape>" . god-local-mode)
+;;   :init
+;;   (setq god-exempt-major-modes nil)
+;;   (setq god-exempt-predicates nil))
+
+(use-package xah-fly-keys
+  :ensure t
+  :pin melpa
+  :config
+  (xah-fly-keys-set-layout "qwerty") ; required if you use qwerty
+										; (xah-fly-set-layout "dvorak") ; by default, it's dvorak
+  (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+  (define-key isearch-mode-map (kbd "TAB") 'isearch-repeat-forward)
+  (xah-fly-keys 1))
 
 ; Specializations for Ergoemacs mode
 (defun insert-commercial-at()
