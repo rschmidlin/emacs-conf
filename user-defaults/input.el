1f8a041d00d10213131f9612a2c6cfe52de3bb49
Raul Schmidlin <raul.schmidlin@logictiles.com>
2018-01-11 21:03:47 +0100

    Modify Boon to have its mode used on special buffers in spite of its special type

--- user-defaults/input.el
+++ user-defaults/input.el
@@ -1,4 +1,4 @@
-;; Select input mode - ergoemacs, xah-fly-keys, boon
+;; Select (insert )nput mode - ergoemacs, xah-fly-keys, boon
 (defvar input-mode "boon")
 
 ; Specializations for system-wide rebind of AltGr to Alt_L
@@ -120,20 +120,31 @@
   (boon-powerline-theme) ;; if you want use powerline with Boon
   (boon-mode)
 
-										; Use M-SPC to go back to command mode
+  ;; Define special modes where boon should be used instead
+  (defvar boon-non-special-list
+  	'(dired-mode))
+  
+  (defun use-special-mode-p (old-function &rest arguments)
+    "Function to substitute boon-special-mode-p and disallow use of special mode for some major-modes"
+  	(and (apply old-function arguments)
+		 (not (memq major-mode boon-non-special-list))))
+  
+  (advice-add #'boon-special-mode-p :around #'use-special-mode-p)
+
+  ; Use M-SPC to go back to command mode
   (global-set-key (kbd "M-SPC") 'boon-set-command-state)
 
-										; Special help keys like pressing escape for C-g, TAB for searching further
-  ;; (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+  ; Special help keys like pressing escape for C-g, TAB for searching further
+  (define-key key-translation-map (kbd "ESC") (kbd "C-g"))
   (define-key isearch-mode-map (kbd "TAB") 'isearch-repeat-forward)
 
-										; Also define commands for C-x that are available from x in Boon
+  ; Also define commands for C-x that are available from x in Boon
   (global-set-key (kbd "C-x o") 'ace-window)
   (global-set-key  (kbd "M-x") 'counsel-M-x)
   (define-key boon-command-map (kbd "x x") 'counsel-M-x)
   (global-set-key (kbd "C-x x") 'counsel-M-x)
 
-										; Define new commands for command mode
+  ; Define new commands for command mode
   (define-key boon-command-map (kbd ",") 'ace-window)
   (define-key boon-command-map (kbd "r") 'swiper)
   (define-key boon-command-map (kbd "m") 'split-window-below)
@@ -143,7 +154,7 @@
   (define-key boon-command-map (kbd "T") 'query-replace)
   (define-key boon-goto-map (kbd "i") 'imenu)
 
-										; New keys on C-x group avoiding necessity of pressing control
+  ; New keys on C-x group avoiding necessity of pressing control
   (global-set-key (kbd "C-x t") 'query-replace-regexp)
   (global-set-key (kbd "C-x ö") 'save-buffer)
   (global-set-key (kbd "C-x j") 'find-file)
@@ -152,13 +163,13 @@
   (global-set-key (kbd "C-x y") 'comment-dwim)
   (global-set-key (kbd "C-x w") 'find-alternate-file)
 
-										; New keys on C-c group avoiding the necessity of pressing control
+  ; New keys on C-c group avoiding the necessity of pressing control
   (defvar boon-help-map)
   (define-prefix-command 'boon-help-map)
   (set-keymap-parent boon-help-map help-map)
   (define-key boon-command-map (kbd "J") boon-help-map)
 
-										; Include extended indexer navigation for Boon
+  ; Include extended indexer navigation for Boon
   (global-set-key (kbd "C-c f") 'ggtags-find-file)
   (global-set-key (kbd "C-c i") 'cscope-find-functions-calling-this-function)
   (global-set-key (kbd "C-c o") 'cscope-find-called-functions)
